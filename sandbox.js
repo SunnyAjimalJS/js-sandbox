e56cdb4d9f0f6ce2b90a0856d822b4f6cf7f6ceb
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-02 15:09:56 +0000

    W3 Schools - Data Types

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,12 @@
-// W3 Schools - JS Operators
+// W3 Schools - JS Data Types
 
-// Multiply 10 with 5, and alert the result:
-alert(10 * 5);
+// Use comments to describe the correct data type of the following variables:
+var length = 16; // Number
+var lastName = "Johnson"; // String
 
-// Divide 10 by 2, and alert the result:
-alert(10 / 2);
+var x = {
+  firstName: "John",
+  lastName: "Doe",
+}; // Object
 
-// Alert the remainder when 15 is divided by 9.
-alert(15 % 9);
-
-// Use the correct assignment operator that will result in x being 15 (same as x = x + y).
-x = 10;
-y = 5;
-x += y;
-
-// Use the correct assignment operator that will result in x being 50 (same as x = x * y).
-x = 10;
-y = 5;
-x *= y;
+//
