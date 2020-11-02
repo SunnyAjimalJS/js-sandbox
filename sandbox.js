27880988514642d5989741c6b5985458cf4eb388
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-02 15:54:44 +0000

    W3 Schools - JS String Methods

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,21 @@
-// W3 Schools - JS Strings
+// W3 Schools - JS String Methods
 
-//Use the length property to alert the length of txt.
-var txt = "Hello World!";
-var x = txt.length;
-alert(x);
+// Find the position of the character h in the string txt.
+var txt = "abcdefghijklm";
+var pos = txt.indexOf("h");
 
-// Use escape characters to alert We are "Vikings".
-// var txt = 'We are \"Vikings\"';
-alert(txt);
+// Use the slice method to return the word "bananas".
+var txt = "I can eat bananas all day";
+var x = txt.slice(10, 17);
 
-// Concatenate the two strings to alert "Hello World!".
-var str1 = "Hello ";
-var str2 = "World!";
-alert(str1 + str2);
+// Use the correct String method to replace the word "Hello" with the word "Welcome".
+var txt = "Hello World";
+txt = txt.replace("Hello", "Welcome");
+
+// Convert the value of txt to upper case.
+var txt = "Hello World";
+txt = txt.toUpperCase();
+
+//Convert the value of txt to lower case.
+var txt = "Hello World";
+txt = txt.toLowerCase();
