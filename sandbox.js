cd087602f53662d412cc6b54eb8c71af9c627d90
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-02 16:43:53 +0000

    W3 Schools - JS Dates

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,17 @@
-// W3 Schools - JS Array Sort
+// W3 Schools - JS Dates
 
-// Use the correct Array method to sort the fruits array alphabetically.
-var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
-fruits.sort();
+// Create a Date object and alert the current date and time.
+var d = new Date();
+alert(d);
+
+// Use the correct Date method to extract the year (four digits) out of a date object.
+var d = new Date();
+year = d.getFullYear();
+
+// Use the correct Date method to get the month (0-11) out of a date object.
+var d = new Date();
+month = d.getMonth();
+
+// Use the correct Date method to set the year of a date object to 2020.
+var d = new Date();
+d.setFullYear(2020);
