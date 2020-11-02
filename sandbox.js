4c2ffaa5b345f727aea2409853356c574b4875a4
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-02 16:37:47 +0000

    W3 Schools - JS Array Sort

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,5 @@
-// W3 Schools - JS Array Methods
+// W3 Schools - JS Array Sort
 
-// Use the correct Array method to remove the last item of the fruits array.
-var fruits = ["Banana", "Orange", "Apple"];
-fruits.pop();
-
-// Use the correct Array method to add "Kiwi" to the fruits array.
-var fruits = ["Banana", "Orange", "Apple"];
-fruits.push("Kiwi");
-
-// Use the splice() method to remove "Orange" and "Apple" from fruits.
+// Use the correct Array method to sort the fruits array alphabetically.
 var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
-fruits.splice(1, 2);
+fruits.sort();
