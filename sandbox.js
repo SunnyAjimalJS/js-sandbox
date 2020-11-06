7d164bb27a1132ce51f755cc1ccd00fc217c2ed8
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-06 12:26:13 +0000

    W3 Schools - JS HTML DOM

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,87 @@
-// W3 Schools - JS Break Loops
+// W3 Schools - JS HTML DOM
 
-// Make the loop stop when i is 5.
-for (i = 0; i < 10; i++) {
-  console.log(i);
-  if (i == 5) {
-    break;
-  }
+// Use the getElementById method to find the <p> element, and change its text to "Hello".
+{
+  /* <div>
+  <p id="demo"></p>
+  <script>document.getElementById("demo").innerHTML= "Hello";</script>
+</div>; */
 }
 
-// Make the loop jump to the next iteration when i is 5.
-for (i = 0; i < 10; i++) {
-  if (i == 5) {
-    continue;
-  }
-  console.log(i);
+// Use the getElementsByTagName method to find the first <p> element, and change its text to "Hello".
+{
+  /* <div>
+  <p id="demo"></p>
+  <script>document.getElementsByTagName("p")[0].innerHTML="Hello";</script>
+</div>; */
+}
+
+// Change the text of the first element that has the class name "test".
+{
+  /* <div>
+  <p class="test"></p>
+  <p class="test"></p>
+
+  <script>
+    document.getElementsByClassName("test")[0].innerHTML= "Hello";
+  </script>
+</div>; */
+}
+
+// Use HTML DOM to change the value of the image's src attribute.
+
+{
+  /* <img id="image" src="smiley.gif">
+
+<script>
+document.getElementById("image").src = "pic_mountain.jpg";
+</script> */
+}
+
+// Use HTML DOM to change the value of the input field.
+
+{
+  /* <input type="text" id="myText" value="Hello">
+
+<script>
+document.getElementById("myText").value = "Have a nice day!";
+</script> */
+}
+
+// Change the text color of the <p> element to "red".
+{
+  /* <p id="demo"></p>
+
+<script>
+document.getElementById("demo").style.color = "red";
+</script> */
+}
+
+// Change the font size of the p element to 40 pixels.
+
+{
+  /* <p id="demo"></p>
+
+<script>
+document.getElementById("demo").style.fontSize = "40px";
+</script> */
+}
+
+// Use the CSS display property to hide the p element.
+{
+  /* <p id="demo"></p>
+
+<script>
+document.getElementById("demo")style.display = "none";
+</script> */
+}
+
+// Use the eventListener to assign an onclick event to the <button> element.
+{
+  /* <button id="demo">Click me1</button>
+
+<script>
+document.getElementById("demo").addEventListener
+("click", myFunction);
+</script> */
 }
