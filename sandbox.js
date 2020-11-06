ba440d3ce56eae25d9d917f058a389b122cecf02
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-06 11:58:46 +0000

    W3 Schools - JS For Loops

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,13 @@
-// W3 Schools - JS Switch
+// W3 Schools - JS For Loops
 
-// Create a switch statement that will alert "Hello" if fruits is "banana", and "Welcome" if fruits is "apple".
-switch (fruits) {
-  case "Banana":
-    alert("Hello");
-    break;
-
-  case "Apple":
-    alert("Welcome");
-    break;
+// Create a loop that runs from 0 to 9.
+var i;
+for (let i = 0; i < 10; i++) {
+  console.log(i);
 }
 
-// Add a section that will alert("Neither") if fruits is neither "banana" nor "apple".
-switch (fruits) {
-  case "Banana":
-    alert("Hello");
-    break;
-  case "Apple":
-    alert("Welcome");
-    break;
-  default:
-    alert("Neither");
+// Create a loop that runs through each item in the fruits array.
+var fruits = ["Apple", "Banana", "Orange"];
+for (x of fruits) {
+  console.log(x);
 }
