cb55723367617b3a0e18f8b3e262084518edab77
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-06 11:23:36 +0000

    W3 Schools - JS Switch

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,24 @@
-// W3 Schools - JS Conditions
+// W3 Schools - JS Switch
 
-// Fix the if statement to alert "Hello World" if x is greater than y.
-if (x > y) {
-  alert("Hello World");
+// Create a switch statement that will alert "Hello" if fruits is "banana", and "Welcome" if fruits is "apple".
+switch (fruits) {
+  case "Banana":
+    alert("Hello");
+    break;
+
+  case "Apple":
+    alert("Welcome");
+    break;
 }
 
-// Fix the if statement to alert "Hello World" if x is greater than y, otherwise alert "Goodbye".
-if (x > y) {
-  alert("Hello World");
-} else {
-  alert("Goodbye");
+// Add a section that will alert("Neither") if fruits is neither "banana" nor "apple".
+switch (fruits) {
+  case "Banana":
+    alert("Hello");
+    break;
+  case "Apple":
+    alert("Welcome");
+    break;
+  default:
+    alert("Neither");
 }
