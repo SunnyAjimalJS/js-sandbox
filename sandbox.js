06b98df52f4eecbaf3d11fb6d2993aeea6f44a12
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-06 12:02:18 +0000

    W3 Schools - JS While Loops

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,15 @@
-// W3 Schools - JS For Loops
+// W3 Schools - JS While Loops
 
-// Create a loop that runs from 0 to 9.
-var i;
-for (let i = 0; i < 10; i++) {
+// Create a loop that runs as long as i is less than 10.
+var i = 0;
+while (i < 10) {
   console.log(i);
+  i++;
 }
 
-// Create a loop that runs through each item in the fruits array.
-var fruits = ["Apple", "Banana", "Orange"];
-for (x of fruits) {
-  console.log(x);
+// Create a loop that runs as long as i is less than 10, but increase i with 2 each time.
+var i = 0;
+while (i < 10) {
+  i = i + 2;
+  console.log(i);
 }
